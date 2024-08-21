ac6f1abf262e9eaa3875cf753595e7f1332f62d5
calaberus <juan.esteban.mondragon@correounivalle.edu.co>
2024-08-21 10:56:20 -0500

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,10 @@
+class Auto:
+    marca="chevrolet"
+    modelo=2024
+    placa="AEW134I"
+taxi=Auto()
+#print(taxi.modelo)
+#rint(taxi.marca)
+#print(taxi.placa)
+#print(taxi.marca,taxi.modelo,taxi.placa)
+print(F"su modelo es={taxi.modelo},su marca es={taxi.marca},y su placa es={taxi.placa}")
